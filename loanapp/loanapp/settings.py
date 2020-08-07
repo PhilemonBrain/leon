1072666cff7737f50a6118ca593bc182f66bdf91
Philbrainy <philbrainy@gmail.com>
2020-08-07 15:11:19 +0100

    requirements

--- loanapp/loanapp/settings.py
+++ loanapp/loanapp/settings.py
@@ -71,15 +71,39 @@ TEMPLATES = [
 WSGI_APPLICATION = 'loanapp.wsgi.application'
 
 AUTH_USER_MODEL = 'app.User'
+AUTHENTICATION_BACKENDS = [
+    # 'accounts.backends.ApiAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'postgres',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'B@thill0449qwik2',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
