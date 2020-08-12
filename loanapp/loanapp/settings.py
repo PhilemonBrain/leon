91265c2b7c99a456d5b5956803c91ef8142c492d
Philbrainy <philbrainy@gmail.com>
2020-08-12 03:37:57 +0100

    auth backend

--- loanapp/loanapp/settings.py
+++ loanapp/loanapp/settings.py
@@ -71,39 +71,39 @@ TEMPLATES = [
 WSGI_APPLICATION = 'loanapp.wsgi.application'
 
 AUTH_USER_MODEL = 'app.User'
-AUTHENTICATION_BACKENDS = [
-    'app.backends.EmailAuthBackend',
-]
+# AUTHENTICATION_BACKENDS = [
+#     'app.backends.EmailAuthBackend',
+# ]
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'postgres',
-
-        'USER': 'postgres',
-
-        'PASSWORD': 'B@thill0449qwik2',
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
 }
+
+# DATABASES = {
+
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+#         'NAME': 'postgres',
+
+#         'USER': 'postgres',
+
+#         'PASSWORD': 'B@thill0449qwik2',
+
+#         'HOST': 'localhost',
+
+#         'PORT': '5432',
+
+#     }
+
+# }
 
 
 # Password validation
