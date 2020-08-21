74d9069e3018452b026a9d75c8be7eab7fa66b4b
onwuagba <onwuagbakenenna@gmail.com>
2020-08-21 03:30:49 +0100

    Feat: Setup Staff and Admin Appsand refactored login

--- loanapp/app/admin.py
+++ loanapp/app/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model as User
-from .models import Staff
+from staff.models import Staff, Branch
 from client.models import Client, Payments, Loans
+from len_admin.models import Admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
 
 admin.site.register(User())
 admin.site.register(Staff)
+admin.site.register(Branch)
 admin.site.register(Client)
 admin.site.register(Payments)
 admin.site.register(Loans)
+admin.site.register(Admin)
+
--- loanapp/loanapp/settings.py
+++ loanapp/loanapp/settings.py
@@ -82,27 +82,53 @@ AUTH_USER_MODEL = 'app.User'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-
-    # 'default': {
-
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-    #     'NAME': 'postgres',
-
-    #     'USER': 'postgres',
-
-    #     'PASSWORD': 'B@thill0449qwik2',
-
-    #     'HOST': 'localhost',
-
-    #     'PORT': '5432',
-
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'leon_db2',
+        'USER': 'adLeonSUser',
+        'PASSWORD': 'B@thill0449qwik2',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+
+#     # 'default': {
+
+#     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+#     #     'NAME': 'postgres',
+
+#     #     'USER': 'postgres',
+
+#     #     'PASSWORD': 'B@thill0449qwik2',
+
+#     #     'HOST': 'localhost',
+
+#     #     'PORT': '5432',
+
+#     # }
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+#         'NAME': 'leon_db2',
+
+#         'USER': 'adLeonSUser',
+
+#         'PASSWORD': 'B@thill0449qwik2',
+
+#         'HOST': 'localhost',
+
+#         'PORT': '5432',
+
+#     }
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+#     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     # }
+# }
 
 
 # Password validation
