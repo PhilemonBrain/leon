8833565dac370930ca40c0ad98dd0727d2b39361
onwuagba <onwuagbakenenna@gmail.com>
2020-09-19 22:04:45 +0100

    delete venv. Please don't delete the gitignore, abeg

--- loanapp/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\cia0410\downloads\kene\python\leon\loanapp\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
